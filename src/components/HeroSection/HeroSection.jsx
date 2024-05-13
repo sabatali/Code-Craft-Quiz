6d70d31122a1082231e1ca300ed8594e20a82d1f
sabatali <maliksabatali@gmail.com>
2024-05-13 10:49:53 +0500

    nwe update

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -15,8 +15,9 @@ const HeroSection = () => {
         </div>
       </div>
       <div class="text-center">
-        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
-        <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">A better way to learn programming</h1>
+        <p class="mt-6 text-lg leading-8 text-gray-600">
+Learning programming effectively involves hands-on practice, embracing mistakes as learning opportunities, and seeking collaboration and feedback for growth.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <Link to={"/all-posts"} class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</Link>
           <a href="https://www.linkedin.com/in/sabat-ali/" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
